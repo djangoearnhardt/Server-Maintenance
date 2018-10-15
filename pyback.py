f70854aaf26785f9e772b8f1a9e2da8c33d1a0dd
djangoearnhardt <flatheadcatfish9@yahoo.com>
2018-10-15 13:35:29 -0600

    Update pyback.py

--- pyback.py
+++ pyback.py
@@ -1,14 +1,14 @@
 import os, shutil
 
 # map network locations
-maa_11 = ('D:\\BBBack')
+maa_13 = ('D:\\BBBack')
 netserv_5 = ('B:\\EE.SQL')
 # sort directory contents into list based on oldest to newest file
-backup_folder = sorted(os.listdir((os.chdir(maa_11))), key=os.path.getctime)                 
+backup_folder = sorted(os.listdir((os.chdir(maa_13))), key=os.path.getctime)                 
 # finds the newest file
 backup_file = backup_folder[-1]
 # copies the newest file to network location
-backup_location = os.path.join(maa_11, backup_file)
+backup_location = os.path.join(maa_13, backup_file)
 shutil.copy2(backup_location, netserv_5)
 
 # pyback.py copies local database daily backup to a network location
