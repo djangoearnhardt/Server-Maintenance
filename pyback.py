bf52fc9ded0228d19a939d727a575bdb6df421f4
djangoearnhardt <flatheadcatfish9@yahoo.com>
2018-10-15 13:33:43 -0600

    Update pyback.py

--- pyback.py
+++ pyback.py
@@ -1,11 +1,15 @@
 import os, shutil
 
+# map network locations
 maa_11 = ('D:\\BBBack')
 netserv_5 = ('B:\\EE.SQL')
-backup_folder = os.listdir(maa_11)                    
+# sort directory contents into list based on oldest to newest file
+backup_folder = sorted(os.listdir((os.chdir(maa_11))), key=os.path.getctime)                 
+# finds the newest file
 backup_file = backup_folder[-1]
+# copies the newest file to network location
 backup_location = os.path.join(maa_11, backup_file)
 shutil.copy2(backup_location, netserv_5)
 
-# pyback.py copies Financial Edge's daily backup to Netserv5
+# pyback.py copies local database daily backup to a network location
 # shutil.copy2 includes meta data and permissions
